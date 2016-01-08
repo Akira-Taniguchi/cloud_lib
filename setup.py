197ed2c03180e6a85fff0ea56d365b946a3a16ac
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2016-01-08 19:30:02 +0900

    update description

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name='cloud_lib',
     version='1.0.2',
     description='Cloud Service Library',
-    long_description='Cloud Service Library',
+    long_description='https://pypi.python.org/pypi/cloud_lib',
     url='https://github.com/Akira-Taniguchi/cloud_lib',
     author='AkiraTaniguchi',
     author_email ='dededededaiou2003@yahoo.co.jp',
     packages=find_packages(),
     license='MIT',
     keywords='amazon google cloud bigquery storage s3 ec2',
-    classifiers = [
+    classifiers=[
       'Development Status :: 5 - Production/Stable',
       'Programming Language :: Python :: 2.7',
       'Intended Audience :: Developers',
